d3087c8905cc958ece973b832ef43ed65716cf84
penyu <penyu@amazon.com>
2014-09-22 15:47:46 +0000

    add more test and pass

--- tests/test_mixin.py
+++ tests/test_mixin.py
@@ -28,22 +28,88 @@ class TestMixin(TestCase):
         self.MixinB = MixinB
         @mixin(MixinA)
         class MixinC_A(Mixin):
-            def func_mixin_C(self):
+            def func_mixin_c(self):
                 return 'do_func_mixin_c'
         self.MixinC_A = MixinC_A
+        class MixinShadowA(Mixin):
+            def func_mixin_a(self):
+                return 'do_func_mixin_shadow_a'
+        self.MixinShadowA = MixinShadowA
+
     def test_instantiation_simple_mixin(self):
         with self.assertRaises(InstantiationMixinError):
             self.MixinA()
+
     def test_instantiation_inherit_mixin(self):
         with self.assertRaises(InstantiationMixinError):
             self.MixinC_A()
-    def test_inherit_simple_mixin(self):
+
+    def test_inherit_simple_mixin_to_normal(self):
+        with self.assertRaises(InheritMixinError):
+            class A(self.MixinA): pass
+
+    def test_inherit_inherit_mixin_to_normal(self):
+        with self.assertRaises(InheritMixinError):
+            class A(self.MixinC_A): pass
+
+    def test_using_simple_mixin(self):
         @mixin(self.MixinA)
-        class A(object):
-            pass
+        class A(object): pass
         self.assertTrue(hasattr(A, 'func_mixin_a'))
         a = A()
         self.assertEqual('do_func_mixin_a', a.func_mixin_a())
-    def test_inherit_mixin(self):
-        with self.assertRaises(InheritMixinError):
-            class A(self.MixinA): pass
+
+    def test_using_inherit_mixin(self):
+        @mixin(self.MixinC_A)
+        class A(object): pass
+        self.assertTrue(hasattr(A, 'func_mixin_a'))
+        self.assertTrue(hasattr(A, 'func_mixin_c'))
+        a = A()
+        self.assertEqual('do_func_mixin_a', a.func_mixin_a())
+        self.assertEqual('do_func_mixin_c', a.func_mixin_c())
+
+    def test_multi_level_mixin(self):
+        @mixin(self.MixinB)
+        @mixin(self.MixinA)
+        class A(object): pass
+        self.assertTrue(hasattr(A, 'func_mixin_a'))
+        self.assertTrue(hasattr(A, 'func_mixin_b'))
+        a = A()
+        self.assertEqual('do_func_mixin_a', a.func_mixin_a())
+        self.assertEqual('do_func_mixin_b', a.func_mixin_b())
+
+    def test_single_level_multi_mixin(self):
+        @mixin(self.MixinA, self.MixinB)
+        class A(object): pass
+        self.assertTrue(hasattr(A, 'func_mixin_a'))
+        self.assertTrue(hasattr(A, 'func_mixin_b'))
+        a = A()
+        self.assertEqual('do_func_mixin_a', a.func_mixin_a())
+        self.assertEqual('do_func_mixin_b', a.func_mixin_b())
+
+    def test_multi_level_shadow(self):
+        @mixin(self.MixinShadowA)
+        class A(object): pass
+        self.assertTrue(hasattr(A, 'func_mixin_a'))
+        a = A()
+        self.assertEqual('do_func_mixin_shadow_a', a.func_mixin_a())
+
+        @mixin(self.MixinShadowA)
+        @mixin(self.MixinA)
+        class A(object): pass
+        self.assertTrue(hasattr(A, 'func_mixin_a'))
+        a = A()
+        self.assertEqual('do_func_mixin_a', a.func_mixin_a())
+
+    def test_single_level_shadow(self):
+        @mixin(self.MixinShadowA)
+        class A(object): pass
+        self.assertTrue(hasattr(A, 'func_mixin_a'))
+        a = A()
+        self.assertEqual('do_func_mixin_shadow_a', a.func_mixin_a())
+
+        @mixin(self.MixinA,self.MixinShadowA)
+        class A(object): pass
+        self.assertTrue(hasattr(A, 'func_mixin_a'))
+        a = A()
+        self.assertEqual('do_func_mixin_a', a.func_mixin_a())
