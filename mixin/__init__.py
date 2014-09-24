d24161e382443f3789637d2ea2eb211589ad5a79
penyu <penyu@amazon.com>
2014-09-24 00:01:58 +0000

    move to top level

--- mixin/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-__all__ = ['mixin', 'Mixin', 'InstantiationMixinError', 'InvalidMixinError', 'InheritMixinError']
-
-from .mixin import mixin, Mixin, InstantiationMixinError, InvalidMixinError, InheritMixinError
